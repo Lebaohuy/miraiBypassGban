e7544691096b513a542e1bbd65513fbadb9335cb
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-07-31 23:35:49 +0700

    Create ad.js

---
+++ modules/commands/ad.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Bảo Huy",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : Lê Bảo Huy\nBiệt Danh: ERICK ~~\nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Cái gì cái chứ trêu gái là nhanh nhất nhé OwO\nChiều cao : 1m65\nSinh ngày : 21/9/2005\nLiên hệ: +84986139357\nSở Thích: Thích chơi game, trêu gái\nCân nặng: 50kg\nID FACEBOOK : 100041358902536\nName ID : Lê Bảo Huy \nLink Facebook : https://www.facebook.com/IRIK.Profiler\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== LÊ BẢO HUY ===`, event.threadID, event.messageID);
