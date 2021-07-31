f2e64477ed74a852f746be2372b938389f14d3df
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-01 00:33:09 +0700

    Update rankup.js

--- modules/commands/rankup.js
+++ modules/commands/rankup.js
@@ -1,60 +1,74 @@
 module.exports.config = {
 	name: "rankup",
-	version: "0.0.1-beta",
+	version: "0.0.2",
 	hasPermssion: 1,
-	credits: "CatalizCS",
+	credits: "Mirai Team",
 	description: "Thông báo rankup cho từng nhóm, người dùng",
 	commandCategory: "system",
-	usages: "rankup",
-	dependencies: ["fs-extra"],
+	dependencies: {
+		"fs-extra": ""
+	},
 	cooldowns: 5,
 	envConfig: {
+		autoUnsend: true,
 		unsendMessageAfter: 5
 	}
 };
 
-module.exports.event = async function({ api, event, Currencies, Users, client }) {
+module.exports.handleEvent = async function({ api, event, Currencies, Users }) {
 	const {threadID, senderID } = event;
-	const { createReadStream, existsSync, mkdirSync } = require("fs-extra");
+	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
 
-	const threadData = client.threadSetting.get(threadID.toString()) || {};
-
-	if (typeof threadData["rankup"] != "undefined" && threadData["rankup"] == false) return;
-	if (client.inProcess == true) return;
+	const thread = global.data.threadData.get(parseInt(threadID)) || {};
 
 	var exp = parseInt((await Currencies.getData(senderID)).exp);
 	exp = exp += 1;
 
 	if (isNaN(exp)) return;
 
+	if (typeof thread["rankup"] != "undefined" && thread["rankup"] == false) {
+		await Currencies.setData(senderID, { exp });
+		return;
+	};
+
 	const curLevel = Math.floor((Math.sqrt(1 + (4 * exp / 3) + 1) / 2));
 	const level = Math.floor((Math.sqrt(1 + (4 * (exp + 1) / 3) + 1) / 2));
 
 	if (level > curLevel && level != 1) {
-		const nameUser = (await Users.getData(senderID)).name || (await Users.getInfo(senderID)).name;
-		var messsage = (typeof threadData.customRankup == "undefined") ? msg = "Trình độ chém gió của {name} đã đạt tới level {level}" : msg = threadData.customRankup,
+		const name = global.data.userName.get(senderID) || await Users.getNameUser(senderID);
+		var messsage = (typeof thread.customRankup == "undefined") ? msg = "Trình độ chém gió của {name} đã đạt tới level {level}" : msg = thread.customRankup,
 			arrayContent;
 
 		messsage = messsage
-			.replace(/\{name}/g, nameUser)
+			.replace(/\{name}/g, name)
 			.replace(/\{level}/g, level);
 			
 		if (existsSync(__dirname + "/cache/rankup/")) mkdirSync(__dirname + "/cache/rankup/", { recursive: true });
-		if (existsSync(__dirname + `/cache/rankup/${event.threadID}.gif`)) arrayContent = { body: messsage, attachment: createReadStream(__dirname + `/cache/rankup/${event.threadID}.gif`), mentions: [{ tag: nameUser, id: senderID }] };
-		else arrayContent = { body: messsage, mentions: [{ tag: nameUser, id: senderID }] };
-		(arrayContent, threadID);
+		if (existsSync(__dirname + `/cache/rankup/${event.threadID}.gif`)) arrayContent = { body: messsage, attachment: createReadStream(__dirname + `/cache/rankup/${event.threadID}.gif`), mentions: [{ tag: name, id: senderID }] };
+		else arrayContent = { body: messsage, mentions: [{ tag: name, id: senderID }] };
+		const moduleName = this.config.name;
+		(arrayContent, threadID, async function (error, info){
+			if (global.configModule[moduleName].autoUnsend) {
+				console.log(global.configModule[moduleName].autoUnsend);
+				await new Promise(resolve => setTimeout(resolve, global.configModule[moduleName].unsendMessageAfter * 1000));
+				return (info.messageID);
+			} else return;
+		});
 	}
 
 	await Currencies.setData(senderID, { exp });
 	return;
 }
-module.exports.run = async function({ api, event, Threads, client }) {
-	let settings = (await Threads.getData(event.threadID)).settings;
-	if (typeof settings["rankup"] == "undefined" || settings["rankup"] == false) settings["rankup"] = true;
-	else settings["rankup"] = false;
+module.exports.run = async function({ api, event, Threads }) {
+	const { threadID, messageID } = event;
+
+	var data = (await Threads.getData(threadID)).data;
 	
-	await Threads.setData(event.threadID, options = { settings });
-	client.threadSetting.set(event.threadID, settings);
+	if (typeof data["rankup"] == "undefined" || data["rankup"] == false) data["rankup"] = true;
+	else data["rankup"] = false;
 	
-	return api.sendMessage(`Đã ${(settings["rankup"] == true) ? "bật" : "tắt"} thành công thông báo rankup!`, event.threadID);
+	await Threads.setData(parseInt(threadID), { data });
+	global.data.threadData.set(parseInt(threadID), data);
+	
+	return api.sendMessage(`Đã ${(data["rankup"] == true) ? "bật" : "tắt"} thành công thông báo rankup!`, threadID, messageID);
 }
