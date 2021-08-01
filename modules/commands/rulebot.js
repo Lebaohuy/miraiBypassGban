711c16764d0e0a15a3472db23378ac5bea7009e1
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-01 15:37:54 +0700

    Update rulebot.js

--- modules/commands/rulebot.js
+++ modules/commands/rulebot.js
@@ -1,26 +1,24 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "rulebot",
-	version: "1.0.0",
+name: "Rulebot",
+	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "thọ", 
-	description: "no prefix",
-	commandCategory: "System",
+	credits: "BaoHuy",
+	description: "Rulebot",
+	commandCategory: "Không cần dấu lệnh",
 	usages: "info",
-    cooldowns: 10, 
+	cooldowns: 5,
 };
-
-module.exports.event = async ({ event, api, Currencies,Users, args, utils }) => {
-if (event.body.indexOf("Rulebot")==0 || (event.body.indexOf("rulebot")==0)) {
-		return api.sendMessage(`==============\nLUẬT SỬ DỤNG\n==============\nBot chỉ được tag 1-2 Box nên mọi người chú ý, cuối tuần Admin sẽ lọc box để tiếp tục đón thêm box mới\nP/s: khi số box vượt quá mức thì Ad xin không nhận thêm\nTHANK YOU FOR USING MY ERICK BOT <3` , event.threadID, event.messageID)
-};
-
-
-module.exports.run = async ({ event, api, Currencies, args, utils }) => {
-
-
-
-
-
-
+module.exports.event = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Yêu Bot")==0 || (event.body.indexOf("yêu Bot")==0)) {
+		var msg = {
+				body: "==============\nLUẬT SỬ DỤNG\n==============\nBot chỉ được tag 1-2 Box nên mọi người chú ý, cuối tuần Admin sẽ lọc box để tiếp tục đón thêm box mới\nP/s: khi số box vượt quá mức thì Ad xin không nhận thêm\nTHANK YOU FOR USING MY ERICK BOT <3 ",
+				
+			}
+			api.sendMessage(msg, threadID, messageID);
+		}
 	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
