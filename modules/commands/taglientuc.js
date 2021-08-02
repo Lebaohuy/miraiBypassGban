6127d13e13fed60f7e2f9a5f5d187f87653b5118
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-03 04:46:09 +0700

    Update taglientuc.js

--- modules/commands/taglientuc.js
+++ modules/commands/taglientuc.js
@@ -1,32 +1,39 @@
 module.exports.config = {
-	name: "taglientuc",
-	version: "1.0.0",
-	hasPermssion: 1,
-	credits: "VanHung & Dựa trên demo của NTKhang",
-	description: "Tag liên tục người bạn tag trong 5 lần\nCó thể gọi là gọi hồn người đó",
-	commandCategory: "group",
-	usages: "taglientuc @mention",
-	cooldowns: 5,
-	dependencies: ["fs-extra","axios"]
+    name: "taglientuc",
+    version: "1.0.0",
+    hasPermssion: 1,
+    credits: "VanHung & Dựa trên demo của NTKhang",
+    description: "Tag liên tục người bạn tag trong 5 lần\nCó thể gọi là gọi hồn người đó",
+    commandCategory: "group","Admin",
+    usages: "taglientuc @mention",
+    cooldowns: 10,
+    dependencies: {
+        "fs-extra": "",
+        "axios": ""
+    }
 }
 
 module.exports.run = async function({ api, args, Users, event}) {
-	var mention = Object.keys(event.mentions)[0];
-	if(!mention) return api.sendMessage("Cần phải tag 1 người bạn muốn gọi hồn", event.threadID);
-	let data = await api.getUserInfo(mention);
-	let name = data[parseInt(mention)].name;
-	var arraytag = [];
-		arraytag.push({id: mention, tag: name});
-	var a = function (a) { api.sendMessage(a, event.threadID); }
-a("Bắt đầu tag!");
-setTimeout(() => {a({body: "Alo con lợn" + " " + name, mentions: arraytag})} , 3000);
-setTimeout(() => {a({body: "Có người cần gặp mày kìa đĩ ơi" + " " + name, mentions: arraytag})} , 7000);
-setTimeout(() => {a({body: "Hiện hồn đi con lợn này" + " " + name, mentions: arraytag})} , 11000);
-setTimeout(() => {a({body: "Địt con mẹ mày đâu rồi hả con chó" + " " + name, mentions: arraytag})} , 15000);
-setTimeout(() => {a({body: "Vào mess đi có người kiếm kìa thằng lồn" + " " + name, mentions: arraytag})} , 20000);
-setTimeout(() => {a({body: "Đĩ mẹ mày có hiện hồn không hả con chó " + " " + name, mentions: arraytag})} , 24000);
-setTimeout(() => {a({body: "Hiện lên đi mà thằng mặt lồn" + " " + name, mentions: arraytag})} , 28000);
-setTimeout(() => {a({body: "Đéo hiện là ăn cức chó nha" + " " + name, mentions: arraytag})} , 32000);
-setTimeout(() => {a({body: "Đĩ mẹ mày không hiện thật à con chó" + " " + name, mentions: arraytag})} , 36000);
-setTimeout(() => {a({body: "Đéo hiện nữa là bay fb nha con chó" + " " + name, mentions: arraytag})} , 40000);
-	}
+    var mention = Object.keys(event.mentions)[0];
+    if(!mention) return api.sendMessage("Cần phải tag 1 người bạn muốn gọi hồn", event.threadID);
+    let data = await api.getUserInfo(mention);
+    let name = data[parseInt(mention)].name;
+    var arraytag = [];
+        arraytag.push({id: mention, tag: name});
+    var a = function (a) { api.sendMessage(a, event.threadID); }
+a("Bắt đầu tag !");
+setTimeout(() => {a({body: "Alo con lợn"+" "+name, mentions: arraytag}, )} , 3000);
+setTimeout(() => {a({body: "Alo alo"+" "+name, mentions: arraytag}, )} , 5000);
+setTimeout(() => {a({body: "Hiện hồn"+" "+name, mentions: arraytag}, )} , 7000);
+setTimeout(() => {a({body: "Ai tìm mày kìa aloo"+" "+name, mentions: arraytag}, )} , 9000);
+setTimeout(() => {a({body: "Có người gặp mày kìa đĩ ơi"+" "+name, mentions: arraytag}, )} , 12000);
+setTimeout(() => {a({body: "Hiện hồn đi con lợn này"+" "+name, mentions: arraytag}, )} , 15000);
+setTimeout(() => {a({body: "Hiện hồn nhanh không người ta đốt nhà mày giờ"+" "+name, mentions: arraytag}, )} , 17000);
+setTimeout(() => {a({body: "Mày đâu rồi có người tìm gấp kìa"+" "+name, mentions: arraytag}, )} , 20000);
+setTimeout(() => {a({body: "Aloooooooooo trả lời đi alo alo"+" "+name, mentions: arraytag}, )} , 23000);
+setTimeout(() => {a({body: "Con cặc địt mẹ mày"+" "+name, mentions: arraytag}, )} , 25000);
+setTimeout(() => {a({body: "Tao đéo tag nữa mệt rồi, pai pai"+" "+name, mentions: arraytag}, )} , 28500);
+setTimeout(() => {a({body: "Tao dỗi rồi"+" "+name, mentions: arraytag}, )} , 31000);
+setTimeout(() => {a({body: "Gửi thành công đến"+" "+name, mentions: arraytag}, )} , 36000);
+  
+  }
