6e733c8e0d60cb38514331c538ce122dbdd51cc7
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-11 00:34:16 +0700

    Update help.js

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -1,72 +1,47 @@
 module.exports.config = {
 	name: "help",
-	version: "1.0.2",
+	version: "1.1.2",
 	hasPermssion: 0,
-	credits: "Mirai Team",
-	description: "Hướng dẫn cho người mới",
-	commandCategory: "system",
-	usages: "[Tên module]",
-	cooldowns: 5,
-	envConfig: {
-		autoUnsend: true,
-		delayUnsend: 40
-	}
+	credits: "HelyT",
+	description: "Help giống Spermbot :3",
+	commandCategory: "group",
+	usages: "[lệnh]",
+	cooldowns: 1,
 };
-
-module.exports.languages = {
-	"vi": {
-		"moduleInfo": "「 %1 」\n%2\n\n❯ Cách sử dụng: %3\n❯ Thuộc nhóm: %4\n❯ Thời gian chờ: %5 giây(s)\n❯ Quyền hạn: %6\n\n» Module code by Lê Bảo Huy «",
-		"helpList": '[ 𝐡𝐢𝐞̣̂𝐧 đ𝐚𝐧𝐠 𝐜𝐨́ %1 𝐥𝐞̣̂𝐧𝐡 𝐜𝐨́ 𝐭𝐡𝐞̂̉ 𝐬𝐮̛̉ 𝐝𝐮̣𝐧𝐠 𝐭𝐫𝐞̂𝐧 𝐛𝐨𝐭 𝐧𝐚̀𝐲, 𝘀𝘂̛̉ 𝗱𝘂̣𝗻𝗴: "%2help nameCommand" đ𝐞̂̉ 𝐱𝐞𝐦 𝐜𝐡𝐢 𝐭𝐢𝐞̂́𝐭 𝐜𝐚́𝐜𝐡 𝐬𝐮̛̉ 𝐝𝐮̣𝐧𝐠!\n𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝘀𝗲̃ 𝘁𝘂̛̣ 𝗴𝗼̛̃ 𝘀𝗮𝘂 𝟯𝟬𝘀🥀 ]"',
-		"user": "Người dùng",
-        "adminGroup": "Quản trị viên nhóm",
-        "adminBot": "Quản trị viên bot"
-	},
-	"en": {
-		"moduleInfo": "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 «",
-		"helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
-		"user": "User",
-        "adminGroup": "Admin group",
-        "adminBot": "Admin bot"
-	}
-}
-
-module.exports.handleEvent = function ({ api, event, getText }) {
+module.exports.handleEvent = function ({ api, event }) {
 	const { commands } = global.client;
+	
+	if (!event.body) return;
+
 	const { threadID, messageID, body } = event;
 
-	if (!body || typeof body == "undefined" || body.indexOf("help") != 0) return;
+	if (body.indexOf("help") != 0) return;
+
 	const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
+
+
 	if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
+
 	const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
 	const command = commands.get(splitBody[1].toLowerCase());
-	const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
-	return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
-}
 
-module.exports.run = function({ api, event, args, getText }) {
-	const { commands } = global.client;
-	const { threadID, messageID } = event;
-	const command = commands.get((args[0] || "").toLowerCase());
-	const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-	const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
 	const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
 
-	if (!command) {
-		const command = commands.values();
-		var group = [], msg = "";
-		for (const commandConfig of command) {
-			if (!group.some(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase())) group.push({ group: commandConfig.config.commandCategory.toLowerCase(), cmds: [commandConfig.config.name] });
-			else group.find(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase()).cmds.push(commandConfig.config.name);
-		}
-		group.forEach(commandGroup => msg += `🥀 ${commandGroup.group.charAt(0).toUpperCase() + commandGroup.group.slice(1)} 🥀\n${commandGroup.cmds.join(', ')}\n\n`);
-		return api.sendMessage(msg + getText("helpList", commands.size, prefix), threadID, async (error, info) =>{
-			if (autoUnsend) {
-				await new Promise(resolve => setTimeout(resolve, delayUnsend * 1000));
-				return api.unsendMessage(info.messageID);
-			} else return;
-		});
+	return api.sendMessage(`⚔️ ${command.config.name} ⚔️\n${command.config.description}\n\n❯ Cách sử dụng: ${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}\n❯ Thuộc nhóm: ${command.config.commandCategory}\n❯ Thời gian chờ: ${command.config.cooldowns} giây(s)\n❯ Quyền hạn: ${((command.config.hasPermssion == 0) ? "Người dùng" : (command.config.hasPermssion == 1) ? "Quản trị viên" : "Người vận hành bot" )}\n❯ Prefix: ${prefix}\n\n» Module code by ${command.config.credits} «`, threadID, messageID);
+};
 
-	}
-
-	return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+module.exports.run = async function({ api, args, Users, event, Threads, utils, client }) {
+const { commands } = global.client;
+const { threadID, messageID } = event;
+const command = commands.get((args[0] || "").toLowerCase());
+const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+if (!command) {
+const command = commands.values();
+var tl = ["hiện tại code này là do Lbhuy code ở phiên bản 1.1.2","con bot này thông minh hơn bạn","bạn đã biết","tôi không có khả năng hiểu con gái","con bot này giúp bạn hỗ trợ trong việc học?","spam bot tôi sẽ ban bạn khỏi người dùng bot","đừng để tôi cáu nhé!","việc bạn đang làm là vô nghĩa","bạn đã làm tôi cáu😡","tôi yêu bạn vai lon","bạn có yêu tôi không ?","cái gì chưa biết chỉ cần biết là được","con chuột bị ốm uống thuốc chuột thì tại sao con chuột lại chết ?","chảy máu cam nhưng sao màu máu là màu đỏ ?","đây chỉ là sản phẩm fake spermbot?","Tôi không có khả năng hiểu được Lòng Người","Ngày 21 tháng 9 là ngày sinh nhật của Admin!","Con bot này giống Spermbot nhưng thật chất chỉ là mod lại ?","Đây là một bản hoàn chỉnh hơn của Mirai V1.","Đây không phải là bot C3C hay KB2A.","228922 là một con số tuyệt vời.","Đây là một lệnh vô dụng","177013 là một con số tuyệt vời","Đã từng có 600+ code JAV ở phiên bản đầu tiên của SpermBot","Ngôn ngữ của Bảo Huy là ngôn ngữ của chúa","Nếu bạn gặp 1 người có tên là Lê Bảo Huy hãy tránh xa người đó càng nhiều càng tốt. Nếu không cả gia phả nhà người đó sẽ ám bạn suốt đời, con cháu bạn sẽ bị ám bởi cái tên Lê Bảo Huy ","Đây là con bot tự viết code cho chính nó","7749 là con số đẹp cho tình yêu","bạn có yêu tôi không ?","bạn rất ngu :))"];
+var tle = tl[Math.floor(Math.random() * tl.length)];
+var lon = `[Bạn có biết?]: ${tle}.`;
+return api.sendMessage(lon, event.threadID, event.messageID);
 }
+const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+return api.sendMessage(`⚔️ ${command.config.name} ⚔️\n${command.config.description}\n\n❯ Cách sử dụng: ${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}\n❯ Thuộc nhóm: ${command.config.commandCategory}\n❯ Thời gian chờ: ${command.config.cooldowns} giây(s)\n❯ Quyền hạn: ${((command.config.hasPermssion == 0) ? "Người dùng" : (command.config.hasPermssion == 1) ? "Quản trị viên" : "Người vận hành bot" )}\n❯ Prefix: ${prefix}\n\n» Module code by ${command.config.credits} «`, threadID, messageID);
+};
