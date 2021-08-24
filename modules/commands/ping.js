f321e9a96d0615b929324558afe9afd6110d468a
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-24 20:46:54 +0700

    Update ping.js

--- modules/commands/ping.js
+++ modules/commands/ping.js
@@ -16,7 +16,7 @@ module.exports.run = async function({ api, event, args }) {
 		global.moduleData["afk"] && global.moduleData["afk"].afkList ? listAFK = Object.keys(global.moduleData["afk"].afkList || []) : listAFK = []; 
 		listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
 		listUserID = listUserID.filter(item => !listAFK.includes(item));
-		var body = (args.length != 0) ? args.join(" ") : "@everyone", mentions = [], index = 0;
+		var body = (args.length != 0) ? args.join(" ") : "@Vợ ơi hiện lên anh bảo 😘", mentions = [], index = 0;
 		
 		for(const idUser of listUserID) {
 			body = "‎" + body;
